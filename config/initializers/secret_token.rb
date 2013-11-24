d23c47a4a8fc680888b8ef9b29f7dea3a3746452
jcaster <joshua.caster@gmail.com>
2013-11-23 21:30:15 -0600

    Dynamically create secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,30 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bitcoinnow::Application.config.secret_key_base = '02a740ac03d3e7c02d4e40d4c41ac0f8a3d50e0f9d9b4c328be32ae12f8c2a2d54426f89c1fef32ae6be0ab3a1fc815c960e8af67a7f045180d5e64d3d7ab2f6'
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Bitcoinnow::Application.config.secret_key_base = secure_token
